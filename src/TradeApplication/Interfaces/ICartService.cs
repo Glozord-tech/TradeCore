3b0210e785ff7c756e0c397a1171539ce4592ae0
Glozord-tech <lukstop622@gmail.com>
2026-08-27 18:18:19 +0300

    Start of CRUD Cart

---
+++ src/TradeApplication/Interfaces/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeApplication.Interfaces
+{
+    internal interface ICartService
+    {
+    }
+}
---
+++ src/TradeInfrastructure/Repository/CartRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeInfrastructure.Repository
+{
+    internal class CartRepository
+    {
+    }
+}
